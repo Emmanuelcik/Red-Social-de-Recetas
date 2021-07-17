5d93b6e28a3df948d745cd6afd02b476852a5ec9
Jesús Emmanuel López Guerrero <jesusemm010@hotmail.com>
2021-07-16 23:33:52 -0500

    Añadiendo el input para agregar fotos

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -47,7 +47,8 @@ class RecetaController extends Controller
             "titulo" => "required|min:6",
             "categoria" => "required",
             "preparacion" => "required",
-            "ingredientes" => "required"
+            "ingredientes" => "required",
+            // "imagen" => "required|image"
         ]);
         DB::table("recetas")->insert([
             "titulo" => $data["titulo"]
--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -62,7 +62,7 @@
                     <label for="ingredientes">Ingredientes</label>
                     <input type="hidden" name="ingredientes" value=" {{ old("ingredientes") }}">
                     <trix-editor input="ingredientes" 
-                    class="form-control @error("preparacion") is-invalid @enderror"></trix-editor>
+                    class="form-control @error("ingredientes") is-invalid @enderror"></trix-editor>
                     @error("ingredientes")
                         <span class="invalid-feedback d-block" role="alert"> 
                             <strong> {{$message}} </strong>
@@ -70,6 +70,15 @@
                     @enderror
                 </div>
 
+                <div class="form-group mt-3">
+                    <label for="imagen">Elige  la imagen</label>
+                    <input id="imagen" type="file" class="form-control @error("imagen") is-invalid @enderror" name="imagen">
+                    @error("imagen")
+                        <span class="invalid-feedback d-block" role="alert"> 
+                            <strong> {{$message}} </strong>
+                        </span>
+                    @enderror
+                </div>
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" value="Agregar Receta">
                 </div>
