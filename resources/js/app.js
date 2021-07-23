dfcce3988c670595737faf6127d36b872e9a5f87
Jesús Emmanuel López Guerrero <jesusemm010@hotmail.com>
2021-07-22 23:14:46 -0500

    Agregué al dependencia de sweetalert para ahcer alertas utilice la alerta necesaria de sweetalert en el js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import VueSweetalert2 from "vue-sweetalert2";
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 require('./bootstrap');
 
@@ -18,8 +20,10 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+Vue.use(VueSweetalert2);
 Vue.config.ignoredElements = ["trix-editor", "trix-toolbars"]
 Vue.component("fecha-receta", require("./components/Fecha.vue").default);
+Vue.component("eliminar-receta", require("./components/EliminarReceta.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
