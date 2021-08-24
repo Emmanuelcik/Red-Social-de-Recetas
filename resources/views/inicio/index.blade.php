c9ccb41cb7c4bb526f25bd1c595ef4aa19aca6f9
Jesús Emmanuel López Guerrero <jesusemm010@hotmail.com>
2021-08-23 22:25:09 -0500

    Se muestra en la pantalla principal las 5 ultimas recetas, se le hizo una maquetación y estilización de tipo card a cada receta mostrada

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -5,6 +5,24 @@
 @endsection
 
 @section('content')
-    <h2 class="titulo-categoria text-uppercase mt-5 mb-4 ">Últimas recetas</h2>
-    {{$nuevas}}
+    
+    <div class="container nuevas-recetas"> 
+        <h2 class="titulo-categoria text-uppercase mt-5 mb-4 ">Últimas recetas</h2>
+        <div class="row">
+            @foreach ($nuevas as $nueva)
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="/storage/{{ $nueva->imagen}}" class="card-img-top" alt="Imagen receta mas reciente">
+                    <div class="card-body">
+                        <h3> {{ Str::title( $nueva->titulo )}} </h3>
+                        <p>{{ Str::words( strip_tags($nueva->preparacion) , 20) }} </p>
+                        <a href="{{route("recetas.show", ["receta" => $nueva->id])}}" class="btn btn-primary d-block font-weight-bold text-uppercase">
+                            Ver receta
+                        </a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
