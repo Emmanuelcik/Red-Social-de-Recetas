8b1b3c53d1a249b4a54c5d0fda7c844f94580434
Jesús Emmanuel López Guerrero <jesusemm010@hotmail.com>
2021-08-26 12:00:44 -0500

    estilos del buscador

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -4,10 +4,25 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" integrity="sha512-UTNP5BXLIptsaj5WdKFrkFov94lDx+eBvbKyoe1YAfjeRPC+gT5kyZ10kOHCfNZqEui1sxmqvodNUx3KbuYI/A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
 
+@section('hero')
+    <div class="hero-categorias">
+        <form  class="container h-100" action="">
+            <div class="row h-100 align-items-center">
+                <div class="col-md-4 texto-buscar">
+                    <p class="display-4">Encuentra una receta para tu proxima comida</p>
+                    <input type="search"
+                            name="buscar"
+                            class="form-control"
+                            placeholder="Buscar receta">
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
+
 @section('content')
-    <img  alt="imagen de fondo" class="hero-categorias"> 
-    {{-- src="{{ asset("/images/bgimagen.jpg")}}" --}}
-    <div class="container nuevas-recetas"> 
+    {{-- <img  alt="imagen de fondo" class="hero-categorias" src="{{ asset("/images/bgimagen.jpg")}}"> --}}
+        <div class="container nuevas-recetas"> 
         <h2 class="titulo-categoria text-uppercase mb-4 ">Últimas recetas</h2>
         <div class="owl-carousel owl-theme">
             @foreach ($nuevas as $nueva)
