da20e32a3d9b0ee4aa2e1cde4b91df0c364f5257
Jesús Emmanuel López Guerrero <jesusemm010@hotmail.com>
2021-08-25 23:51:18 -0500

    Se puede acceder por medio de la nevegacion de categorias a cada una de las recetas ordenado por categoria

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,8 @@ Route::get("/perfiles/{perfil}", "PerfilController@show")->name("perfiles.show")
 Route::get("/perfiles/{perfil}/edit", "PerfilController@edit")->name("perfiles.edit");
 Route::put("/perfiles/{perfil}", "PerfilController@update")->name("perfiles.update");
 
-
+//RECETAS POR CATEGORIA 
+Route::get("/categoria/{categoriaReceta}", "CategoriasController@show")->name("categorias.show");
 //Mandar likes a la bd (Almacena likes de las recetas)
 Route::post('/recetas/{receta}', 'LikesController@update')->name("likes.update");
 Auth::routes();
